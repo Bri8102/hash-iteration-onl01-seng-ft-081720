82312a3a83b7a1707d2b7b8f1b0d766c4b26cf5e
Briana Oniru <damioniru16@gmail.com>
2020-08-21 06:39:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,11 +11,9 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
+   birthdayy = []
    birthday_kids.each do |kids_name, age|
-     if age == 6
-       puts "Happy Birthday #{kids_name}! You are now #{age} years
-old!"
-     elsif age == 9
+     if age <= 12
        puts "Happy Birthday #{kids_name}! You are now #{age} years
 old!"
     else
